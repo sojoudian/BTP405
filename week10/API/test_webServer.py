6093d71a224eb919ea699a41fc72c3b584b81f3e
Maziar Sojoudian <maziar@sojoudian.net>
2024-03-14 12:24:39 -0400

    add test to github

--- week10/API/test_webServer.py
+++ week10/API/test_webServer.py
@@ -1,4 +1,3 @@
-from webServer import SimpleHTTPRequestHandler
 import unittest
 from unittest.mock import patch
 from urllib.request import urlopen
@@ -17,6 +16,15 @@ class TestWebServer(unittest.TestCase):
 
         self.assertEqual(json.loads(response), {"message": "This is a GET request"})
 
+    @patch('urllib.request.urlopen')
+    def test_post_endpoint(self, mock_urlopen):
+        # Mock the response for the POST request
+        mock_response = json.dumps({"message": "POST request received", "data": "test data"}).encode('utf-8')
+        mock_urlopen.return_value.read.return_value = mock_response
+        mock_urlopen.return_value.__enter__.return_value = mock_urlopen.return_value
 
+        data = urlencode({'some': 'data'}).encode()
+        response = urlopen('http://localhost:8000/post-endpoint', data=data ).read()
+        self.assertEqual(json.loads(response), {"message": "POST request received", "some": "data")
 if __name__ == 'main':
     unittest.main()
