ecf2aff7b29ccd3f0eca94a635ba91c40372d75b
Maziar Sojoudian <maziar@sojoudian.net>
2024-03-14 11:06:46 -0400

    add first simple test

---
+++ week10/test_add.py
@@ -0,0 +1,13 @@
+import unittest
+
+def add(a, b):
+    return a + b
+
+class TestAddFunction(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
+
+if __name__ == '__main__':
+    unittest.main()
